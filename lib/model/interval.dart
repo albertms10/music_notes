43d4b00573e83a1d0bfbba0eda1c9f51bd28f03f
albert <albertmasa2@gmail.com>
2020-06-02 19:41:06 +0200

    Added incorrect Interval assertion

--- lib/model/interval.dart
+++ lib/model/interval.dart
@@ -1,6 +1,6 @@
+import 'package:music_notes_relations/model/enums/enums_to_string.dart';
 import 'package:music_notes_relations/model/enums/intervals.dart';
 import 'package:music_notes_relations/model/enums/qualities.dart';
-import 'package:music_notes_relations/model/enums/enums_to_string.dart';
 
 class Interval {
   final Intervals interval;
@@ -8,7 +8,14 @@ class Interval {
 
   Interval(this.interval, this.quality)
       : assert(interval != null),
-        assert(quality != null);
+        assert(quality != null),
+        assert(
+          (interval == Intervals.Quarta ||
+                  interval == Intervals.Quinta ||
+                  interval == Intervals.Octava)
+              ? (quality != Qualities.Major || quality != Qualities.Menor)
+              : true,
+        );
 
   @override
   String toString() => '${interval.toText()} ${quality.toText()}';
