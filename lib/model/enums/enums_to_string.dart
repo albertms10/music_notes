084eb8f90e0c9fec62c98b1b76e4173be989998e
albert <albertmasa2@gmail.com>
2020-06-02 19:25:41 +0200

    Added Modes and Qualities toString extensions

--- lib/model/enums/enums_to_string.dart
+++ lib/model/enums/enums_to_string.dart
@@ -1,6 +1,8 @@
 import 'package:music_notes_relations/model/enums/accidentals.dart';
 import 'package:music_notes_relations/model/enums/intervals.dart';
+import 'package:music_notes_relations/model/enums/modes.dart';
 import 'package:music_notes_relations/model/enums/notes.dart';
+import 'package:music_notes_relations/model/enums/qualities.dart';
 
 extension NotesToString on Notes {
   String toText() => toString().split('.')[1];
@@ -10,6 +12,14 @@ extension AccidentalsToString on Accidentals {
   String toText() => toString().split('.')[1];
 }
 
+extension ModesToString on Modes {
+  String toText() => toString().split('.')[1];
+}
+
 extension IntervalsToString on Intervals {
+  String toText() => toString().split('.')[1];
+}
+
+extension QualitiesToString on Qualities {
   String toText() => toString().split('.')[1];
 }
