668270c43836bce88721c9a609c55d57cf7abc84
albert <albertmasa2@gmail.com>
2020-06-02 13:45:53 +0200

    Fixed music_test

--- test/music_test.dart
+++ test/music_test.dart
@@ -1,3 +1,4 @@
+import 'package:music_notes_relations/model/enharmonic_note.dart';
 import 'package:music_notes_relations/model/enums/accidentals.dart';
 import 'package:music_notes_relations/model/enums/notes.dart';
 import 'package:music_notes_relations/model/mixins/music.dart';
@@ -9,47 +10,47 @@ void main() {
     expect(
         Music.chromaticScale,
         equals([
-          [
+          EnharmonicNote([
             Note(Notes.Do),
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.Do, Accidentals.Sostingut),
             Note(Notes.Re, Accidentals.Bemoll)
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.Re),
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.Re, Accidentals.Sostingut),
             Note(Notes.Mi, Accidentals.Bemoll)
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.Mi),
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.Fa),
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.Fa, Accidentals.Sostingut),
             Note(Notes.Sol, Accidentals.Bemoll)
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.Sol),
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.Sol, Accidentals.Sostingut),
             Note(Notes.La, Accidentals.Bemoll)
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.La),
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.La, Accidentals.Sostingut),
             Note(Notes.Si, Accidentals.Bemoll),
-          ],
-          [
+          ]),
+          EnharmonicNote([
             Note(Notes.Si),
-          ]
+          ])
         ]));
   });
 }
