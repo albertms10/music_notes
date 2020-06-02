cbf1f9b5dac5610713a1b6b8b995e6540471467d
albert <albertmasa2@gmail.com>
2020-06-02 14:50:59 +0200

    Moved FifthsCircle

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:music_notes_relations/model/enharmonic_note.dart';
+import 'package:music_notes_relations/model/enums/notes.dart';
 import 'package:music_notes_relations/model/mixins/music.dart';
 
 void main() {
-  print(Music.chromaticScale);
-  print(FifthsCircle.fifthsCircle);
-}
-
-class FifthsCircle {
-  static List<EnharmonicNote> get fifthsCircle {
-    final notes = <EnharmonicNote>[];
-
-    for (int i = 0; i < Music.chromaticDivisions * 7; i += 7)
-      notes.add(Music.chromaticScale[Music.modValue(i)]);
-
-    return notes;
-  }
-
-  static int fifthsDistance(EnharmonicNote note1, EnharmonicNote note2) {}
+  print(NotesValues.note(2));
 }
---
+++ lib/model/fifths_circle.dart
@@ -0,0 +1,15 @@
+import 'package:music_notes_relations/model/enharmonic_note.dart';
+import 'package:music_notes_relations/model/mixins/music.dart';
+
+class FifthsCircle {
+  static List<EnharmonicNote> get fifthsCircle {
+    final notes = <EnharmonicNote>[];
+
+    for (int i = 0; i < Music.chromaticDivisions * 7; i += 7)
+      notes.add(Music.chromaticScale[Music.modValue(i)]);
+
+    return notes;
+  }
+
+  static int fifthsDistance(EnharmonicNote note1, EnharmonicNote note2) {}
+}
