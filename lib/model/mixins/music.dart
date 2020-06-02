b79ee28d99416a15632ef4b28b88d598314ef904
albert <albertmasa2@gmail.com>
2020-06-02 17:23:31 +0200

    Fixed modValue calcs and changed order in class

--- lib/model/mixins/music.dart
+++ lib/model/mixins/music.dart
@@ -3,11 +3,13 @@ import 'package:music_notes_relations/model/enharmonic_note.dart';
 mixin Music {
   static final int chromaticDivisions = 12;
 
-  static int modValue(int value) =>
-      value == chromaticDivisions ? value : value % chromaticDivisions;
-
   static final chromaticScale = [
     for (int i = 1; i <= Music.chromaticDivisions; i++)
       EnharmonicNote.fromValue(i)
   ];
+
+  static int modValue(int value) {
+    int modValue = value % chromaticDivisions;
+    return modValue == 0 ? chromaticDivisions : modValue;
+  }
 }
