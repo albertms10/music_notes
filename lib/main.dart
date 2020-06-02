55eedf37ea12be897ec328c910737073f7423937
albert <albertmasa2@gmail.com>
2020-06-02 13:29:41 +0200

    Moved chromaticScale static method to Music

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,16 @@ import 'package:music_notes_relations/model/enharmonic_note.dart';
 import 'package:music_notes_relations/model/mixins/music.dart';
 
 void main() {
-  print(FifthsCircle.chromaticScale);
+  print(Music.chromaticScale);
   print(FifthsCircle.fifthsCircle);
 }
 
 class FifthsCircle {
-  static final chromaticScale = [
-    for (int i = 1; i <= Music.chromaticDivisions; i++)
-      EnharmonicNote.fromValue(i)
-  ];
-
   static List<EnharmonicNote> get fifthsCircle {
     final notes = <EnharmonicNote>[];
 
     for (int i = 0; i < Music.chromaticDivisions * 7; i += 7)
-      notes.add(chromaticScale[i % chromaticScale.length]);
+      notes.add(Music.chromaticScale[i % Music.chromaticScale.length]);
 
     return notes;
   }
--- lib/model/mixins/music.dart
+++ lib/model/mixins/music.dart
@@ -1,6 +1,13 @@
+import 'package:music_notes_relations/model/enharmonic_note.dart';
+
 mixin Music {
   static int chromaticDivisions = 12;
 
   static int modValue(int value) =>
       value == chromaticDivisions ? value : value % chromaticDivisions;
+
+  static final chromaticScale = [
+    for (int i = 1; i <= Music.chromaticDivisions; i++)
+      EnharmonicNote.fromValue(i)
+  ];
 }
