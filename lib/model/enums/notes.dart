27711f00afc4012c7bfb77658f419f40054ebb42
albert <albertmasa2@gmail.com>
2020-06-02 12:54:06 +0200

    Refactored noteValues constant Map

--- lib/model/enums/notes.dart
+++ lib/model/enums/notes.dart
@@ -1,23 +1,17 @@
 enum Notes { Do, Re, Mi, Fa, Sol, La, Si }
 
 extension NotesValues on Notes {
-  static Notes note(int value) => const {
-        1: Notes.Do,
-        3: Notes.Re,
-        5: Notes.Mi,
-        6: Notes.Fa,
-        8: Notes.Sol,
-        10: Notes.La,
-        12: Notes.Si,
-      }[value];
+  static const noteValues = {
+    1: Notes.Do,
+    3: Notes.Re,
+    5: Notes.Mi,
+    6: Notes.Fa,
+    8: Notes.Sol,
+    10: Notes.La,
+    12: Notes.Si,
+  };
 
-  int get value => const {
-        Notes.Do: 1,
-        Notes.Re: 3,
-        Notes.Mi: 5,
-        Notes.Fa: 6,
-        Notes.Sol: 8,
-        Notes.La: 10,
-        Notes.Si: 12,
-      }[this];
+  static Notes note(int value) => noteValues[value];
+
+  int get value => noteValues.keys.firstWhere((element) => this == element);
 }
