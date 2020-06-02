9ed989d18781ae3e087da2ef043f59078e186ab8
albert <albertmasa2@gmail.com>
2020-06-02 13:29:48 +0200

    Changed test

--- test/music_test.dart
+++ test/music_test.dart
@@ -1,38 +1,55 @@
-import 'package:music_notes_relations/main.dart';
 import 'package:music_notes_relations/model/enums/accidentals.dart';
 import 'package:music_notes_relations/model/enums/notes.dart';
+import 'package:music_notes_relations/model/mixins/music.dart';
 import 'package:music_notes_relations/model/note.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('chromaticScale calc is correct', () {
+  test('Chromatic scale is calculed correctly', () {
     expect(
-        FifthsCircle.chromaticScale,
+        Music.chromaticScale,
         equals([
-          [Note(Notes.Do)],
+          [
+            Note(Notes.Do),
+          ],
           [
             Note(Notes.Do, Accidentals.Sostingut),
             Note(Notes.Re, Accidentals.Bemoll)
           ],
-          [Note(Notes.Re)],
+          [
+            Note(Notes.Re),
+          ],
           [
             Note(Notes.Re, Accidentals.Sostingut),
             Note(Notes.Mi, Accidentals.Bemoll)
           ],
-          [Note(Notes.Mi)],
-          [Note(Notes.Fa)],
+          [
+            Note(Notes.Mi),
+          ],
+          [
+            Note(Notes.Fa),
+          ],
           [
             Note(Notes.Fa, Accidentals.Sostingut),
             Note(Notes.Sol, Accidentals.Bemoll)
           ],
-          [Note(Notes.Sol)],
+          [
+            Note(Notes.Sol),
+          ],
           [
             Note(Notes.Sol, Accidentals.Sostingut),
             Note(Notes.La, Accidentals.Bemoll)
           ],
-          [Note(Notes.La)],
-          [Notes.La, Accidentals.Sostingut, Note(Notes.Si, Accidentals.Bemoll)],
-          [Note(Notes.Si)]
+          [
+            Note(Notes.La),
+          ],
+          [
+            Note(Notes.La, Accidentals.Sostingut),
+            Note(Notes.Si, Accidentals.Bemoll),
+          ],
+          [
+            Note(Notes.Si),
+          ]
         ]));
   });
 }
