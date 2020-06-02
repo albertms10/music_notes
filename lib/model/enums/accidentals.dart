a257f77aca60e440c418bfd09a1126ca13a9e143
albert <albertmasa2@gmail.com>
2020-06-02 14:30:09 +0200

    Refactored accidentalsValues static const and getters

--- lib/model/enums/accidentals.dart
+++ lib/model/enums/accidentals.dart
@@ -1,10 +1,15 @@
 enum Accidentals { DobleSostingut, Sostingut, Bemoll, DobleBemoll }
 
 extension AccidentalsValues on Accidentals {
-  int get value => const {
-        Accidentals.DobleSostingut: 2,
-        Accidentals.Sostingut: 1,
-        Accidentals.Bemoll: -1,
-        Accidentals.DobleBemoll: -2,
-      }[this];
+  static const accidentalValues = {
+    Accidentals.DobleSostingut: 2,
+    Accidentals.Sostingut: 1,
+    Accidentals.Bemoll: -1,
+    Accidentals.DobleBemoll: -2,
+  };
+
+  static Accidentals accidental(int value) => accidentalValues.keys
+      .firstWhere((accidental) => value == accidentalValues[accidental]);
+
+  int get value => accidentalValues[this];
 }
