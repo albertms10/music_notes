d6031fcc6d63e7850326b8fd23818551bfecbedf
albert <albertmasa2@gmail.com>
2020-06-03 22:27:55 +0200

    Cleared main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-import 'package:music_notes_relations/model/enharmonic_note.dart';
-import 'package:music_notes_relations/model/enums/notes.dart';
-import 'package:music_notes_relations/model/mixins/music.dart';
-
 void main() {
-  print(NotesValues.note(2));
+  [].forEach(print);
 }
