f3963058b424c213d3092522725ad159868f4a29
albert <albertmasa2@gmail.com>
2020-06-03 17:21:29 +0200

    Added QualitiesValues extension

--- lib/model/enums/qualities.dart
+++ lib/model/enums/qualities.dart
@@ -1,1 +1,16 @@
 enum Qualities { Augmentada, Major, Justa, Menor, Disminuida }
+
+extension QualitiesValues on Qualities {
+  static const qualitiesDeltas = {
+    Qualities.Disminuida,
+    Qualities.Menor,
+    Qualities.Major,
+    Qualities.Augmentada,
+  };
+
+  static const perfectQualitiesDeltas = {
+    Qualities.Disminuida,
+    Qualities.Justa,
+    Qualities.Augmentada,
+  };
+}
