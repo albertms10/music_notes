9f1532397e774b46cd733e42c3851ff8a6bc28cb
albert <albertmasa2@gmail.com>
2020-06-03 23:56:26 +0200

    Reorganized classes in its own directory

--- lib/music_notes.dart
+++ lib/music_notes.dart
@@ -2,17 +2,17 @@ library music_notes;
 
 import 'dart:math' as math;
 
-part "src/circle_of_fifths.dart";
-part "src/enharmonic_interval.dart";
-part "src/enharmonic_note.dart";
+part "src/classes/circle_of_fifths.dart";
+part "src/classes/enharmonic_interval.dart";
+part "src/classes/enharmonic_note.dart";
 part "src/enums/accidentals.dart";
 part "src/enums/enums_to_string.dart";
 part "src/enums/intervals.dart";
 part "src/enums/modes.dart";
 part "src/enums/notes.dart";
 part "src/enums/qualities.dart";
-part "src/interval.dart";
-part "src/key_signature.dart";
+part "src/classes/interval.dart";
+part "src/classes/key_signature.dart";
 part "src/mixins/music.dart";
-part "src/note.dart";
-part "src/tonality.dart";
+part "src/classes/note.dart";
+part "src/classes/tonality.dart";
