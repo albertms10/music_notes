6d473727fbaae1bb4b130e9e74d4aa5e48a6551e
albert <albertmasa2@gmail.com>
2020-06-03 17:40:34 +0200

    Fixed Music chromaticDivisions const declaration

--- lib/model/mixins/music.dart
+++ lib/model/mixins/music.dart
@@ -1,7 +1,7 @@
 import 'package:music_notes_relations/model/enharmonic_note.dart';
 
 mixin Music {
-  static final int chromaticDivisions = 12;
+  static const int chromaticDivisions = 12;
 
   static final chromaticScale = {
     for (int i = 1; i <= Music.chromaticDivisions; i++)
