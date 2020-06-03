142f0616f910a7147876223ef50239f667cc7adc
albert <albertmasa2@gmail.com>
2020-06-03 18:38:35 +0200

    Improved Intervals test use of bool values

--- test/intervals_test.dart
+++ test/intervals_test.dart
@@ -3,16 +3,16 @@ import 'package:test/test.dart';
 
 void main() {
   test('Perfect intervals definitions are correct', () {
-    expect(Intervals.Unison.isPerfect, true);
-    expect(Intervals.Segona.isPerfect, false);
-    expect(Intervals.Tercera.isPerfect, false);
-    expect(Intervals.Quarta.isPerfect, true);
-    expect(Intervals.Quinta.isPerfect, true);
-    expect(Intervals.Sexta.isPerfect, false);
-    expect(Intervals.Septima.isPerfect, false);
-    expect(Intervals.Octava.isPerfect, true);
-    expect(Intervals.Novena.isPerfect, false);
-    expect(Intervals.Onzena.isPerfect, true);
-    expect(Intervals.Tretzena.isPerfect, false);
+    expect(Intervals.Unison.isPerfect, isTrue);
+    expect(Intervals.Segona.isPerfect, isFalse);
+    expect(Intervals.Tercera.isPerfect, isFalse);
+    expect(Intervals.Quarta.isPerfect, isTrue);
+    expect(Intervals.Quinta.isPerfect, isTrue);
+    expect(Intervals.Sexta.isPerfect, isFalse);
+    expect(Intervals.Septima.isPerfect, isFalse);
+    expect(Intervals.Octava.isPerfect, isTrue);
+    expect(Intervals.Novena.isPerfect, isFalse);
+    expect(Intervals.Onzena.isPerfect, isTrue);
+    expect(Intervals.Tretzena.isPerfect, isFalse);
   });
 }
