4dd28f2115ddcef60514e727cacd7bdb096ed749
albert <albertmasa2@gmail.com>
2020-06-03 19:36:47 +0200

    Refactored hardcoded values

--- lib/model/circle_of_fifths.dart
+++ lib/model/circle_of_fifths.dart
@@ -1,6 +1,9 @@
 import 'dart:math' as math;
 
 import 'package:music_notes_relations/model/enharmonic_note.dart';
+import 'package:music_notes_relations/model/enums/intervals.dart';
+import 'package:music_notes_relations/model/enums/qualities.dart';
+import 'package:music_notes_relations/model/interval.dart';
 import 'package:music_notes_relations/model/mixins/music.dart';
 import 'package:music_notes_relations/model/note.dart';
 
@@ -8,7 +11,9 @@ class CircleOfFifths {
   static Set<EnharmonicNote> get fifthsCircle {
     final notes = <EnharmonicNote>{};
 
-    for (int i = 0; i < Music.chromaticDivisions * 7; i += 7)
+    var fifthSt = Interval(Intervals.Quinta, Qualities.Justa).semitones;
+
+    for (int i = 0; i < Music.chromaticDivisions * fifthSt; i += fifthSt)
       notes.add(Music.chromaticScale.toList()[Music.modValueWithZero(i)]);
 
     return notes;
@@ -16,14 +21,17 @@ class CircleOfFifths {
 
   static int shortestFifthsDistance(
       EnharmonicNote enharmonicNote1, EnharmonicNote enharmonicNote2) {
+    var fifthSt = Interval(Intervals.Quinta, Qualities.Justa).semitones;
+
     final int distanceAbove =
-        enharmonicNote1.enharmonicSemitonesDistance(enharmonicNote2, 7);
+        enharmonicNote1.enharmonicSemitonesDistance(enharmonicNote2, fifthSt);
     final int distanceBelow =
-        enharmonicNote1.enharmonicSemitonesDistance(enharmonicNote2, -7);
+        enharmonicNote1.enharmonicSemitonesDistance(enharmonicNote2, -fifthSt);
 
     return math.min(distanceAbove, distanceBelow);
   }
 
   static int exactFifthsDistance(Note note1, Note note2) =>
-      note1.intervalDistance(note2, 7);
+      note1.intervalDistance(
+          note2, Interval(Intervals.Quinta, Qualities.Justa).semitones);
 }
