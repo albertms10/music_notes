80d8c0ff7df007fc42d68b1e63991a26bcac2242
albert <albertmasa2@gmail.com>
2020-06-04 23:19:40 +0200

    Added EnharmonicNote doc comments

--- lib/src/classes/enharmonic_note.dart
+++ lib/src/classes/enharmonic_note.dart
@@ -9,8 +9,21 @@ class EnharmonicNote {
   EnharmonicNote.fromValue(int value)
       : this(_enharmonicNoteFromValue(value).enharmonicNotes);
 
+  /// Returns the value of the common chromatic pitch of this [EnharmonicNote].
+  ///
+  /// ```dart
+  /// EnharmonicNote({
+  ///   Note(Notes.Re, Accidental.Bemoll),
+  ///   Note(Notes.Do, Accidental.Sostingut),
+  /// }).value == 2
+  ///
+  /// EnharmonicNote.fromValue(4).value == 4
+  /// ```
   int get value => enharmonicNotes.toList()[0].value;
 
+  /// Returns the [EnharmonicNote] from a given [value].
+  ///
+  /// It is mainly used by [EnharmonicNote.fromValue] constructor.
   static EnharmonicNote _enharmonicNoteFromValue(int value) {
     final note = NotesValues.note(value);
 
@@ -40,6 +53,15 @@ class EnharmonicNote {
     });
   }
 
+  /// Returns the [Note] from a given [value] and a [preferredAccidental].
+  ///
+  /// ```dart
+  /// EnharmonicNote.getNote(4, Accidentals.Sostingut)
+  ///   == Note(Notes.Re, Accidentals.Sostingut)
+  ///
+  /// EnharmonicNote.getNote(5, Accidentals.Bemoll)
+  ///   == Note(Notes.Fa, Accidentals.Bemoll)
+  /// ```
   static Note getNote(int value, [Accidentals preferredAccidental]) {
     var enharmonicNotes = EnharmonicNote.fromValue(value).enharmonicNotes;
 
@@ -52,6 +74,16 @@ class EnharmonicNote {
     );
   }
 
+  /// Returns the shortest iteration distance from [enharmonicNote]
+  /// to [semitones].
+  ///
+  /// ```dart
+  /// EnharmonicNote({Note(Notes.Sol)})
+  ///   .enharmonicSemitonesDistance(
+  ///     EnharmonicNote.fromValue(10),
+  ///     7,
+  ///   ) == 2
+  /// ```
   int enharmonicSemitonesDistance(
     EnharmonicNote enharmonicNote,
     int semitones,
@@ -69,9 +101,34 @@ class EnharmonicNote {
     return distance;
   }
 
+  /// Returns the shortest iteration distance from [enharmonicNote]
+  /// to [interval].
+  ///
+  /// ```dart
+  /// EnharmonicNote.fromValue(5)
+  ///   .enharmonicIntervalDistance(
+  ///     EnharmonicNote({Note(Notes.Re)}),
+  ///     Interval(Intervals.Quinta, Qualities.Justa),
+  ///   ) == 10
+  ///
+  /// EnharmonicNote.fromValue(5)
+  ///   .enharmonicIntervalDistance(
+  ///     EnharmonicNote({Note(Notes.Re)}),
+  ///     Interval(Intervals.Quinta, Qualities.Justa, descending: true),
+  ///   ) == 2
+  /// ```
   int enharmonicIntervalDistance(EnharmonicNote note, Interval interval) =>
       enharmonicSemitonesDistance(note, interval.semitones);
 
+  /// Returns a transposed [EnharmonicNote] by [semitones] from this [EnharmonicNoste].
+  ///
+  /// ```dart
+  /// EnharmonicNote({Note(Notes.Do)}).transposeBy(7)
+  ///   == EnharmonicNote({
+  ///     Note(Notes.Fa, Accidentals.Sostingut),
+  ///     Note(Notes.Sol, Accidentals.Bemoll),
+  ///   })
+  /// ```
   EnharmonicNote transposeBy(int semitones) =>
       EnharmonicNote.fromValue(this.value + semitones);
 
