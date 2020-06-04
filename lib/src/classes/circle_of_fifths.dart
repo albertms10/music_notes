892b0474bb8a1d3c2731005005d46ebf94e1d180
albert <albertmasa2@gmail.com>
2020-06-04 23:37:29 +0200

    Added CircleOfFifths doc comments

--- lib/src/classes/circle_of_fifths.dart
+++ lib/src/classes/circle_of_fifths.dart
@@ -1,6 +1,7 @@
 part of music_notes;
 
 abstract class CircleOfFifths {
+  /// Returns a [Set] of [EnharmonicNotes] that conforms the circle of fifths.
   static Set<EnharmonicNote> get circleOfFifths {
     final notes = <EnharmonicNote>{};
 
@@ -14,6 +15,20 @@ abstract class CircleOfFifths {
     return notes;
   }
 
+  /// Returns the shortest iteration distance between two [EnharmonicNote]s
+  /// in fifth intervals.
+  ///
+  /// ```dart
+  /// CircleOfFifths.shortestFifthsDistance(
+  ///   EnharmonicNote({Note(Notes.Fa, Accidentals.Sostingut)}),
+  ///   EnharmonicNote({Note(Notes.La)})
+  /// ) == -3
+  /// 
+  /// CircleOfFifths.shortestFifthsDistance(
+  ///   EnharmonicNote.fromValue(4),
+  ///   EnharmonicNote.fromValue(8)
+  /// ) == 4
+  /// ```
   static int shortestFifthsDistance(
     EnharmonicNote enharmonicNote1,
     EnharmonicNote enharmonicNote2,
@@ -32,6 +47,20 @@ abstract class CircleOfFifths {
     return minDistance * (minDistance == distanceAbove ? 1 : -1);
   }
 
+  /// Returns the exact iteration distance between two [Note]s
+  /// in fifth intervals.
+  /// 
+  /// ```dart
+  /// CircleOfFifths.exactFifthsDistance(
+  ///   Note(Notes.La, Accidentals.Bemoll),
+  ///   Note(Notes.Do, Accidentals.Sostingut)
+  /// ) == 11
+  /// 
+  /// CircleOfFifths.exactFifthsDistance(
+  ///   Note(Notes.La, Accidentals.Bemoll),
+  ///   Note(Notes.Re, Accidentals.Bemoll)
+  /// ) == -1
+  /// ```
   static int exactFifthsDistance(Note note1, Note note2) =>
       note1.intervalDistance(
         note2,
