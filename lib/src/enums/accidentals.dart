eac19289119ed1ec94650821c76a3a4b7de8bc5d
albert <albertmasa2@gmail.com>
2020-06-04 23:49:18 +0200

    Added tripple Accidentals

--- lib/src/enums/accidentals.dart
+++ lib/src/enums/accidentals.dart
@@ -1,13 +1,22 @@
 part of music_notes;
 
-enum Accidentals { DobleSostingut, Sostingut, Bemoll, DobleBemoll }
+enum Accidentals {
+  TripleSostingut,
+  DobleSostingut,
+  Sostingut,
+  Bemoll,
+  DobleBemoll,
+  TripleBemoll
+}
 
 extension AccidentalsValues on Accidentals {
   static const accidentalValues = {
+    Accidentals.TripleSostingut: 3,
     Accidentals.DobleSostingut: 2,
     Accidentals.Sostingut: 1,
     Accidentals.Bemoll: -1,
     Accidentals.DobleBemoll: -2,
+    Accidentals.TripleBemoll: -3,
   };
 
   static Accidentals accidental(int value) => accidentalValues.keys.firstWhere(
