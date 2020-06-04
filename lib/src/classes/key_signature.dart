8c04deb36a9b9d21fb3a6587d7c67cc098562a6d
albert <albertmasa2@gmail.com>
2020-06-04 17:32:08 +0200

    Added KeySignature fromDistance constructor

--- lib/src/classes/key_signature.dart
+++ lib/src/classes/key_signature.dart
@@ -8,6 +8,13 @@ class KeySignature {
       : assert(number != null),
         assert(accidental != null);
 
+  KeySignature.fromDistance(int distance)
+      : this(
+          distance.abs(),
+          distance > 0 ? Accidentals.Sostingut : Accidentals.Bemoll,
+        );
+
+  // List<Tonality> get tonalities;
 
   @override
   String toString() => '$number × ${accidental.toText()}';
