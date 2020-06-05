6e0fe0a53432e72ce4550301276c01b144fc85df
albert <albertmasa2@gmail.com>
2020-06-05 23:56:25 +0200

    Fixed music_test format

--- test/music_test.dart
+++ test/music_test.dart
@@ -4,49 +4,50 @@ import 'package:test/test.dart';
 void main() {
   test('Chromatic scale is correct', () {
     expect(
-        Music.chromaticScale,
-        equals([
-          EnharmonicNote({
-            const Note(Notes.Do),
-          }),
-          EnharmonicNote({
-            const Note(Notes.Do, Accidentals.Sostingut),
-            const Note(Notes.Re, Accidentals.Bemoll)
-          }),
-          EnharmonicNote({
-            const Note(Notes.Re),
-          }),
-          EnharmonicNote({
-            const Note(Notes.Re, Accidentals.Sostingut),
-            const Note(Notes.Mi, Accidentals.Bemoll)
-          }),
-          EnharmonicNote({
-            const Note(Notes.Mi),
-          }),
-          EnharmonicNote({
-            const Note(Notes.Fa),
-          }),
-          EnharmonicNote({
-            const Note(Notes.Fa, Accidentals.Sostingut),
-            const Note(Notes.Sol, Accidentals.Bemoll)
-          }),
-          EnharmonicNote({
-            const Note(Notes.Sol),
-          }),
-          EnharmonicNote({
-            const Note(Notes.Sol, Accidentals.Sostingut),
-            const Note(Notes.La, Accidentals.Bemoll)
-          }),
-          EnharmonicNote({
-            const Note(Notes.La),
-          }),
-          EnharmonicNote({
-            const Note(Notes.La, Accidentals.Sostingut),
-            const Note(Notes.Si, Accidentals.Bemoll),
-          }),
-          EnharmonicNote({
-            const Note(Notes.Si),
-          })
-        ]));
+      Music.chromaticScale,
+      equals([
+        EnharmonicNote({
+          const Note(Notes.Do),
+        }),
+        EnharmonicNote({
+          const Note(Notes.Do, Accidentals.Sostingut),
+          const Note(Notes.Re, Accidentals.Bemoll)
+        }),
+        EnharmonicNote({
+          const Note(Notes.Re),
+        }),
+        EnharmonicNote({
+          const Note(Notes.Re, Accidentals.Sostingut),
+          const Note(Notes.Mi, Accidentals.Bemoll)
+        }),
+        EnharmonicNote({
+          const Note(Notes.Mi),
+        }),
+        EnharmonicNote({
+          const Note(Notes.Fa),
+        }),
+        EnharmonicNote({
+          const Note(Notes.Fa, Accidentals.Sostingut),
+          const Note(Notes.Sol, Accidentals.Bemoll)
+        }),
+        EnharmonicNote({
+          const Note(Notes.Sol),
+        }),
+        EnharmonicNote({
+          const Note(Notes.Sol, Accidentals.Sostingut),
+          const Note(Notes.La, Accidentals.Bemoll)
+        }),
+        EnharmonicNote({
+          const Note(Notes.La),
+        }),
+        EnharmonicNote({
+          const Note(Notes.La, Accidentals.Sostingut),
+          const Note(Notes.Si, Accidentals.Bemoll),
+        }),
+        EnharmonicNote({
+          const Note(Notes.Si),
+        })
+      ]),
+    );
   });
 }
