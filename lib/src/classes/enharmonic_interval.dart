ad5c84bfbb87a78e35898395c3ff6d42374def28
albert <albertmasa2@gmail.com>
2020-06-05 13:39:02 +0200

    Added EnharmonicInterval fromSemitones constructor

--- lib/src/classes/enharmonic_interval.dart
+++ lib/src/classes/enharmonic_interval.dart
@@ -5,6 +5,41 @@ class EnharmonicInterval extends Enharmonic<Interval> {
 
   EnharmonicInterval(this.intervals) : super(intervals);
 
+  EnharmonicInterval.fromSemitones(int semitones)
+      : this(_fromSemitones(semitones));
+
+  /// Returns the [EnharmonicInterval] from [semitones].
+  ///
+  /// It is mainly used by [EnharmonicInterval.fromSemitones] constructor.
+  static Set<Interval> _fromSemitones(int semitones) {
+    final interval = IntervalsValues.fromSemitones(semitones);
+
+    if (interval != null) {
+      var intervalBelow =
+          IntervalsValues.fromOrdinal(Intervals.values.indexOf(interval));
+      var intervalAbove =
+          IntervalsValues.fromOrdinal(Intervals.values.indexOf(interval) + 2);
+
+      return {
+        if (QualitiesValues.exists(intervalBelow, semitones))
+          Interval.fromDesiredSemitones(intervalBelow, semitones),
+        Interval.fromDesiredSemitones(interval, semitones),
+        if (QualitiesValues.exists(intervalAbove, semitones))
+          Interval.fromDesiredSemitones(intervalAbove, semitones),
+      };
+    }
+
+    var intervalBelow = IntervalsValues.fromSemitones(semitones - 1);
+    var intervalAbove = IntervalsValues.fromSemitones(semitones + 1);
+
+    return {
+      if (QualitiesValues.exists(intervalBelow, semitones))
+        Interval.fromDesiredSemitones(intervalBelow, semitones),
+      if (QualitiesValues.exists(intervalAbove, semitones))
+        Interval.fromDesiredSemitones(intervalAbove, semitones)
+    };
+  }
+
   /// Returns the semitones of the common chromatic pitch of [intervals].
   ///
   /// Example:
