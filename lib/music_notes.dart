655f108d90280a892a11c03ed9d8d1f580b104e1
albert <albertmasa2@gmail.com>
2020-06-05 03:14:16 +0200

    Added MusicItem and Enharmonic abstract classes

--- lib/music_notes.dart
+++ lib/music_notes.dart
@@ -3,11 +3,13 @@ library music_notes;
 import 'dart:math' as math show min;
 
 part "src/classes/circle_of_fifths.dart";
+part "src/classes/enharmonic.dart";
 part "src/classes/enharmonic_interval.dart";
 part "src/classes/enharmonic_note.dart";
 part "src/classes/interval.dart";
 part "src/classes/key_signature.dart";
 part "src/classes/music.dart";
+part "src/classes/music_item.dart";
 part "src/classes/note.dart";
 part "src/classes/tonality.dart";
 part "src/enums/accidentals.dart";
---
+++ lib/src/classes/enharmonic.dart
@@ -0,0 +1,20 @@
+part of music_notes;
+
+abstract class Enharmonic<T extends MusicItem> {
+  final Set<T> items;
+
+  Enharmonic(this.items)
+      : assert(items != null && items.length > 0),
+        assert(
+          items.every(
+            (item) => item.value == itemsValue(items),
+          ),
+          "The items are not enharmonic",
+        );
+
+  /// Returns the value of the common chromatic pitch of [items].
+  static int itemsValue(Set items) => items.toList()[0].value;
+
+  /// Returns the value of the common chromatic pitch this [Enharmonic].
+  int get value => itemsValue(items);
+}
