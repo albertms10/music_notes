e1de2af34d377b325bcddda3412464f29e09bba5
albert <albertmasa2@gmail.com>
2020-06-07 12:09:15 +0200

    Improved theory references in Tonality

--- lib/src/classes/tonality.dart
+++ lib/src/classes/tonality.dart
@@ -25,12 +25,8 @@ class Tonality {
   /// const Tonality(const Note(Notes.Sol), Modes.Menor).accidentals == 2
   /// ```
   int get accidentals => CircleOfFifths.exactFifthsDistance(
-        Note(Notes.Do),
-        mode == Modes.Major
-            ? note
-            : note.transposeByInterval(
-                Interval(Intervals.Tercera, Qualities.Menor),
-              ),
+        Tonality.fromAccidentals(0, mode).note,
+        note,
       ).abs();
 
   /// Returns an [Accidentals] enum item of this [Tonality]’s key signature.
@@ -43,10 +39,13 @@ class Tonality {
   /// const Tonality(const Note(Notes.Fa), Modes.Menor).accidental
   ///   == Accidentals.Bemoll
   /// ```
-  Accidentals get accidental =>
-      CircleOfFifths.exactFifthsDistance(Note(Notes.Do), note) > 0
-          ? Accidentals.Sostingut
-          : Accidentals.Bemoll;
+  Accidentals get accidental => CircleOfFifths.exactFifthsDistance(
+            Tonality.fromAccidentals(0, Modes.Major).note,
+            note,
+          ) >
+          0
+      ? Accidentals.Sostingut
+      : Accidentals.Bemoll;
 
   /// Returns the [Modes.Major] or [Modes.Menor] relative [Tonality] of this [Tonality].
   ///
@@ -78,8 +77,8 @@ class Tonality {
   /// ```
   KeySignature get keySignature => KeySignature.fromDistance(
         CircleOfFifths.exactFifthsDistance(
-          Note(Notes.Do),
-          mode == Modes.Major ? note : this.relative.note,
+          Tonality.fromAccidentals(0, mode).note,
+          note,
         ),
       );
 
