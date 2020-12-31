bed6207de8c2be7fde16e8a1be955a86bd4d04d6
albertms10 <albertmasa2@gmail.com>
2020-12-31 14:27:48 +0100

    refactor(enharmonic): reduce semitones method logic complexity

--- lib/src/classes/enharmonic.dart
+++ lib/src/classes/enharmonic.dart
@@ -7,18 +7,13 @@ abstract class Enharmonic<T extends MusicItem> {
       : assert(items != null && items.isNotEmpty),
         assert(
           items.every(
-            (item) => item.semitones == _itemsSemitones(items),
+            (item) => item.semitones == items.first.semitones,
           ),
           '${T}s are not enharmonic.',
         );
 
-  /// Returns the number of semitones of the common chromatic pitch of [items].
-  ///
-  /// It is used by [semitones] getter.
-  static int _itemsSemitones(Set items) => items.first.semitones;
-
   /// Returns the number of semitones of the common chromatic pitch this [Enharmonic].
-  int get semitones => _itemsSemitones(items);
+  int get semitones => items.first.semitones;
 
   /// Returns a transposed [EnharmonicInterval] by [semitones] from this [EnharmonicInterval].
   Enharmonic transposeBy(int semitones);
