acd2fbd63fa71bb3a54417abbcb1126b913c2f4a
Albert Mañosa <albertmasa2@gmail.com>
2021-06-03 00:57:15 +0200

    refactor(key_signature): simplify boolean assertion expression

--- lib/src/classes/key_signature.dart
+++ lib/src/classes/key_signature.dart
@@ -6,7 +6,7 @@ class KeySignature {
 
   const KeySignature(this.number, [this.accidental])
       : assert(number >= 0),
-        assert(number > 0 ? accidental != null : true);
+        assert(!(number > 0) || accidental != null);
 
   KeySignature.fromDistance(int distance)
       : this(
