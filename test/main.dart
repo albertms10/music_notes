6a79e6993f2952fefc0297b79b175c79c3f2cd28
Albert Mañosa <albertmasa2@gmail.com>
2023-04-08 18:46:44 +0200

    perf(tests): run tests from index `main.dart` file (#20)

---
+++ test/main.dart
@@ -0,0 +1,7 @@
+import 'src/main.dart' as src_test;
+import 'utils/main.dart' as utils_test;
+
+void main() {
+  src_test.main();
+  utils_test.main();
+}
---
+++ test/utils/main.dart
@@ -0,0 +1,5 @@
+import 'int_mod_extension_test.dart' as int_mod_extension_test;
+
+void main() {
+  int_mod_extension_test.main();
+}
