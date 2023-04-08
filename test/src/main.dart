2ef5b73c786227a6c78fa453f5379066672c5d4b
Albert Mañosa <albertmasa2@gmail.com>
2023-04-09 00:22:20 +0200

    test(main): rename `intervals_test`

--- test/src/main.dart
+++ test/src/main.dart
@@ -1,5 +1,5 @@
 import 'interval/enharmonic_interval_test.dart' as enharmonic_interval_test;
-import 'interval/intervals_test.dart' as interval_test;
+import 'interval/intervals_test.dart' as intervals_test;
 import 'music_test.dart' as music_test;
 import 'note/accidental_test.dart' as accidental_test;
 import 'note/enharmonic_note_test.dart' as enharmonic_note_test;
@@ -7,7 +7,7 @@ import 'note/note_test.dart' as note_test;
 
 void main() {
   enharmonic_interval_test.main();
-  interval_test.main();
+  intervals_test.main();
   music_test.main();
   accidental_test.main();
   enharmonic_note_test.main();
