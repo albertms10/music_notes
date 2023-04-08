0496ad86a325ec5e29473356dc52fce81581e06e
Albert Mañosa <albertmasa2@gmail.com>
2023-04-08 17:26:29 +0200

    refactor(src): reorganize directories by content (#15)
    * refactor(src): reorganize directories by content
    
    * refactor(test): reorganize directories by content
    
    * refactor(music_notes): import `iterable.dart` relative

--- lib/music_notes.dart
+++ lib/music_notes.dart
@@ -5,22 +5,23 @@ import 'dart:math' as math show min;
 
 import 'package:collection/collection.dart' show IterableExtension;
 import 'package:meta/meta.dart';
-import 'package:music_notes/src/utils/iterable.dart';
 import 'package:quiver/core.dart';
 
-part 'src/classes/enharmonic.dart';
-part 'src/classes/enharmonic_interval.dart';
-part 'src/classes/enharmonic_note.dart';
-part 'src/classes/interval.dart';
-part 'src/classes/key_signature.dart';
-part 'src/classes/relative_tonalities.dart';
-part 'src/classes/tonality.dart';
-part 'src/enums/intervals.dart';
-part 'src/enums/modes.dart';
-part 'src/enums/notes.dart';
-part 'src/enums/qualities.dart';
-part 'src/interfaces/music_item.dart';
-part 'src/interfaces/transposable.dart';
+import 'src/utils/iterable.dart';
+
+part 'src/enharmonic.dart';
+part 'src/interval/enharmonic_interval.dart';
+part 'src/interval/interval.dart';
+part 'src/interval/intervals.dart';
+part 'src/interval/qualities.dart';
+part 'src/music.dart';
+part 'src/music_item.dart';
 part 'src/note/accidental.dart';
+part 'src/note/enharmonic_note.dart';
 part 'src/note/note.dart';
-part 'src/utils/music.dart';
+part 'src/note/notes.dart';
+part 'src/tonality/key_signature.dart';
+part 'src/tonality/modes.dart';
+part 'src/tonality/relative_tonalities.dart';
+part 'src/tonality/tonality.dart';
+part 'src/transposable.dart';
