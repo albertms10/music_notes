c018ece8b4f0c90b755e37371a49f59a34c46e99
Albert Mañosa <albertmasa2@gmail.com>
2023-04-09 16:35:36 +0200

    feat(num_extension): add `toDeltaString` extension method

---
+++ test/utils/num_extension_test.dart
@@ -0,0 +1,15 @@
+import 'package:music_notes/utils/num_extension.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('NumExtension', () {
+    group('.toDeltaString()', () {
+      test('should return a delta string representation of this num', () {
+        expect(1.1.toDeltaString(), '+1.1');
+        expect(0.toDeltaString(), '+0');
+        expect((-5).toDeltaString(), '-5');
+        expect((-10.02).toDeltaString(), '-10.02');
+      });
+    });
+  });
+}
