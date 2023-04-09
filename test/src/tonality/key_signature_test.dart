65358975db6e4913b6e91dbcf91513c42be9678c
Albert Mañosa <albertmasa2@gmail.com>
2023-04-09 02:45:37 +0200

    test(key_signature): add test cases for `tonalities` getter

--- test/src/tonality/key_signature_test.dart
+++ test/src/tonality/key_signature_test.dart
@@ -3,6 +3,101 @@ import 'package:test/test.dart';
 
 void main() {
   group('KeySignature', () {
+    group('.tonalities', () {
+      test('should return the Set of tonalities for this KeySignature', () {
+        expect(const KeySignature(10, Accidental.flat).tonalities, {
+          // TODO(albertms10): Failing test:
+          //  Should be Note(Notes.e, Accidental.doubleFlat).
+          const Tonality(Note.d, Modes.major),
+          const Tonality(Note(Notes.c, Accidental.flat), Modes.minor),
+        });
+        expect(const KeySignature(9, Accidental.flat).tonalities, {
+          // TODO(albertms10): Failing test:
+          //  Should be Note(Notes.b, Accidental.doubleFlat).
+          const Tonality(Note.a, Modes.major),
+          const Tonality(Note.gFlat, Modes.minor),
+        });
+        expect(const KeySignature(8, Accidental.flat).tonalities, {
+          const Tonality(Note(Notes.f, Accidental.flat), Modes.major),
+          const Tonality(Note.dFlat, Modes.minor),
+        });
+        expect(const KeySignature(7, Accidental.flat).tonalities, {
+          const Tonality(Note(Notes.c, Accidental.flat), Modes.major),
+          const Tonality(Note.aFlat, Modes.minor),
+        });
+        expect(const KeySignature(6, Accidental.flat).tonalities, {
+          const Tonality(Note.gFlat, Modes.major),
+          const Tonality(Note.eFlat, Modes.minor),
+        });
+        expect(const KeySignature(5, Accidental.flat).tonalities, {
+          const Tonality(Note.dFlat, Modes.major),
+          const Tonality(Note.bFlat, Modes.minor),
+        });
+        expect(const KeySignature(4, Accidental.flat).tonalities, {
+          const Tonality(Note.aFlat, Modes.major),
+          const Tonality(Note.f, Modes.minor),
+        });
+        expect(const KeySignature(3, Accidental.flat).tonalities, {
+          const Tonality(Note.eFlat, Modes.major),
+          const Tonality(Note.c, Modes.minor),
+        });
+        expect(const KeySignature(2, Accidental.flat).tonalities, {
+          const Tonality(Note.bFlat, Modes.major),
+          const Tonality(Note.g, Modes.minor),
+        });
+        expect(const KeySignature(1, Accidental.flat).tonalities, {
+          const Tonality(Note.f, Modes.major),
+          const Tonality(Note.d, Modes.minor),
+        });
+        expect(const KeySignature(0).tonalities, {
+          const Tonality(Note.c, Modes.major),
+          const Tonality(Note.a, Modes.minor),
+        });
+        expect(const KeySignature(1, Accidental.sharp).tonalities, {
+          const Tonality(Note.g, Modes.major),
+          const Tonality(Note.e, Modes.minor),
+        });
+        expect(const KeySignature(2, Accidental.sharp).tonalities, {
+          const Tonality(Note.d, Modes.major),
+          const Tonality(Note.b, Modes.minor),
+        });
+        expect(const KeySignature(3, Accidental.sharp).tonalities, {
+          const Tonality(Note.a, Modes.major),
+          const Tonality(Note.fSharp, Modes.minor),
+        });
+        expect(const KeySignature(4, Accidental.sharp).tonalities, {
+          const Tonality(Note.e, Modes.major),
+          const Tonality(Note.cSharp, Modes.minor),
+        });
+        expect(const KeySignature(5, Accidental.sharp).tonalities, {
+          const Tonality(Note.b, Modes.major),
+          const Tonality(Note.gSharp, Modes.minor),
+        });
+        expect(const KeySignature(6, Accidental.sharp).tonalities, {
+          const Tonality(Note.fSharp, Modes.major),
+          const Tonality(Note.dSharp, Modes.minor),
+        });
+        expect(const KeySignature(7, Accidental.sharp).tonalities, {
+          const Tonality(Note.cSharp, Modes.major),
+          const Tonality(Note.aSharp, Modes.minor),
+        });
+        expect(const KeySignature(8, Accidental.sharp).tonalities, {
+          const Tonality(Note.gSharp, Modes.major),
+          const Tonality(Note(Notes.e, Accidental.sharp), Modes.minor),
+        });
+        expect(const KeySignature(9, Accidental.sharp).tonalities, {
+          const Tonality(Note.dSharp, Modes.major),
+          const Tonality(Note(Notes.b, Accidental.sharp), Modes.minor),
+        });
+        expect(const KeySignature(10, Accidental.sharp).tonalities, {
+          const Tonality(Note.aSharp, Modes.major),
+          // TODO(albertms10): Failing test:
+          //  Should be Note(Notes.f, Accidental.doubleSharp).
+          const Tonality(Note.g, Modes.minor),
+        });
+      });
+    });
+
     group('.toString()', () {
       test(
         'should return the string representation of this KeySignature',
