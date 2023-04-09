a52bd464056abb834a816cfaac1ca9c9e37ba054
Albert Mañosa <albertmasa2@gmail.com>
2023-04-09 17:31:15 +0200

    test(enharmonic): add test cases for `toString` method

---
+++ test/src/enharmonic_test.dart
@@ -0,0 +1,24 @@
+import 'package:music_notes/music_notes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Enharmonic', () {
+    group('.toString()', () {
+      test('should return a string representation of this Enharmonic', () {
+        expect(EnharmonicNote.c.toString(), '1 {C, D𝄫, B♯}');
+        expect(EnharmonicNote.g.toString(), '8 {F𝄪, G, A𝄫}');
+        expect(EnharmonicNote.dSharp.toString(), '4 {D♯, E♭}');
+
+        expect(
+          const EnharmonicInterval(1).toString(),
+          '1 {perfect unison, diminished second}',
+        );
+        expect(
+          const EnharmonicInterval(5).toString(),
+          '5 {major third, diminished fourth}',
+        );
+        expect(const EnharmonicInterval(9).toString(), '9 {minor sixth}');
+      });
+    });
+  });
+}
