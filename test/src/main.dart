2c4234663712c702cbe12d64cc6b05d791cde511
Albert Mañosa <albertmasa2@gmail.com>
2023-04-15 12:58:47 +0200

    test(note): add test cases for `difference` (#55)
    * test(note): add test cases for `difference`
    
    * docs(note): add examples for `difference`
    
    * test(main): call Notes `main` function
    
    Missing from #54

--- test/src/main.dart
+++ test/src/main.dart
@@ -8,6 +8,7 @@ import 'music_test.dart' as music_test;
 import 'note/accidental_test.dart' as accidental_test;
 import 'note/enharmonic_note_test.dart' as enharmonic_note_test;
 import 'note/note_test.dart' as note_test;
+import 'note/notes_test.dart' as notes_test;
 import 'tonality/key_signature_test.dart' as key_signature_test;
 import 'tonality/modes_test.dart' as modes_test;
 import 'tonality/tonality_test.dart' as tonality_test;
@@ -22,6 +23,7 @@ void main() {
   accidental_test.main();
   enharmonic_note_test.main();
   note_test.main();
+  notes_test.main();
   key_signature_test.main();
   modes_test.main();
   tonality_test.main();
