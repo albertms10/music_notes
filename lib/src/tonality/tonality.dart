b40cbe64eae65a580a7339353fd89aac33f79c74
Albert Mañosa <albertmasa2@gmail.com>
2023-05-09 23:56:02 +0200

    refactor(tonality): use `Interval.descending` method

--- lib/src/tonality/tonality.dart
+++ lib/src/tonality/tonality.dart
@@ -68,10 +68,7 @@ class Tonality implements Comparable<Tonality> {
   /// ```
   Tonality get relative => Tonality(
         note.transposeBy(
-          Interval.imperfect(
-            3 * (mode == TonalMode.major ? -1 : 1),
-            ImperfectQuality.minor,
-          ),
+          Interval.minorThird.descending(isDescending: mode == TonalMode.major),
         ),
         mode.opposite,
       );
