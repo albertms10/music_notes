ebfb747fe057c6a50e313b5a7744537b6bb44bf0
Albert Mañosa <albertmasa2@gmail.com>
2023-05-19 00:41:05 +0200

    feat(frequency): add `+`, `-`, `*`, `/` operators (#130)
    * feat(frequency): add `+`, `-`, `*`, `/` operators
    
    * refactor(positioned_note): use `*` operator on Frequency

--- test/src/note/frequency_test.dart
+++ test/src/note/frequency_test.dart
@@ -21,6 +21,57 @@ void main() {
       });
     });
 
+    group('operator +()', () {
+      test('should add other to this Frequency', () {
+        expect(
+          const Frequency(0) + const Frequency(1200),
+          const Frequency(1200),
+        );
+        expect(
+          const Frequency(277.18) + const Frequency(415.3),
+          const Frequency(692.48),
+        );
+        expect(
+          const Frequency(440) + const Frequency(220),
+          const Frequency(660),
+        );
+      });
+    });
+
+    group('operator -()', () {
+      test('should subtract other from this Frequency', () {
+        expect(
+          const Frequency(20000.12) - const Frequency(0),
+          const Frequency(20000.12),
+        );
+        expect(
+          const Frequency(415.3) - const Frequency(277.18),
+          const Frequency(138.12),
+        );
+        expect(
+          const Frequency(440) - const Frequency(220),
+          const Frequency(220),
+        );
+      });
+    });
+
+    group('operator *()', () {
+      test('should multiply this Frequency by factor', () {
+        expect(const Frequency(467) * 0, const Frequency(0));
+        expect(const Frequency(2200.2) * 1, const Frequency(2200.2));
+        expect(const Frequency(415.3) * 2, const Frequency(830.6));
+        expect(const Frequency(440) * 0.5, const Frequency(220));
+      });
+    });
+
+    group('operator /()', () {
+      test('should divide this Frequency by factor', () {
+        expect(const Frequency(467) / 1, const Frequency(467));
+        expect(const Frequency(415.3) / 2, const Frequency(207.65));
+        expect(const Frequency(440) / 0.5, const Frequency(880));
+      });
+    });
+
     group('.toString()', () {
       test('should return the string representation of this Frequency', () {
         expect(const Frequency(440).toString(), '440.0 Hz');
