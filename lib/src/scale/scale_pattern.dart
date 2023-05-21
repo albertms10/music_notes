1d7470ceb8247f38f6c32f9df7d53469c115aea4
Albert Mañosa <albertmasa2@gmail.com>
2023-05-22 01:37:24 +0200

    docs(scale_pattern): include explanatory comment on `reversed` operation in `from`

--- lib/src/scale/scale_pattern.dart
+++ lib/src/scale/scale_pattern.dart
@@ -224,6 +224,9 @@ final class ScalePattern {
           [scalable],
           (scale, interval) => [...scale, scale.last.transposeBy(interval)],
         ),
+        // We iterate over the `reversed` descending step list to make sure both
+        // regular and descending scales match, e.g., their octave in
+        // `PositionedNote` lists.
         _descendingIntervalSteps?.reversed
             .fold(
               [scalable],
