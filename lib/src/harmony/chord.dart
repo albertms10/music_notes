90f4d9507402ccf7037cf51c4d638e749466cd8e
Albert Mañosa <albertmasa2@gmail.com>
2023-05-29 11:07:45 +0200

    feat(chord): implement `Transposable`

--- lib/src/harmony/chord.dart
+++ lib/src/harmony/chord.dart
@@ -1,6 +1,6 @@
 part of '../../music_notes.dart';
 
-class Chord<T extends Scalable<T>> {
+class Chord<T extends Scalable<T>> implements Transposable<Chord<T>> {
   /// The [Scalable<T>] items this [Chord] is built of.
   final List<T> items;
 
@@ -22,6 +22,10 @@ class Chord<T extends Scalable<T>> {
         // We skip the root of the chord.
         for (final scalable in items.skip(1)) root.interval(scalable),
       ]);
+
+  @override
+  Chord<T> transposeBy(Interval interval) =>
+      Chord(items.map((item) => item.transposeBy(interval)).toList());
 
   @override
   String toString() => '$root ${pattern.abbreviation} (${items.join(' ')})';
