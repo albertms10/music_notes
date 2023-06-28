57b912c4f8023ac9596ef54072cd00c5ca00dfc1
Albert Mañosa <albertmasa2@gmail.com>
2023-06-29 00:31:07 +0200

    :recycle: refactor(music): remove unused variable

--- lib/src/music.dart
+++ lib/src/music.dart
@@ -1,6 +1,4 @@
 part of '../music_notes.dart';
 
-/// Number of chromatic divisions in an octave.
+/// Number of chromatic divisions in [TuningSystem.edo12].
 const int chromaticDivisions = 12;
-
-final double sqrt12_2 = math.pow(2, 1 / 12).toDouble();
