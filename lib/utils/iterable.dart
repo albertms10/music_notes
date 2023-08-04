6f5d18c1ed493108d6cb2de9916f89d85448a36c
Albert Mañosa <albertmasa2@gmail.com>
2023-08-04 18:20:38 +0200

    :recycle: refactor(iterable): rename `comparator` variable

--- lib/utils/iterable.dart
+++ lib/utils/iterable.dart
@@ -2,8 +2,8 @@
 int compareMultiple(Iterable<int Function()> comparators) {
   assert(comparators.length > 1, 'Provide more than one comparator');
   late int compareValue;
-  for (final comparable in comparators) {
-    compareValue = comparable();
+  for (final comparator in comparators) {
+    compareValue = comparator();
     if (compareValue != 0) break;
   }
 
