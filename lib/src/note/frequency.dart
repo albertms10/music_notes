b250960547fea1637aa7c02d8c04ff02a1e45629
Albert Mañosa <albertmasa2@gmail.com>
2023-10-12 12:42:34 +0200

    docs(note): :book: improve methods documentation

--- lib/src/note/frequency.dart
+++ lib/src/note/frequency.dart
@@ -22,8 +22,7 @@ class Frequency implements Comparable<Frequency> {
   /// ```dart
   /// const Frequency(880).isHumanAudible == true
   /// Note.a.inOctave(4).frequency().isHumanAudible == true
-  /// Note.g.inOctave(12).frequency(const Frequency(442))
-  ///   .isHumanAudible == false
+  /// Note.g.inOctave(12).frequency().isHumanAudible == false
   /// ```
   bool get isHumanAudible {
     const minFrequency = 20;
@@ -103,13 +102,13 @@ class Frequency implements Comparable<Frequency> {
   ///
   /// Example:
   /// ```dart
-  /// Note.a.inOctave(3).frequency().harmonics(upTo: 2)
+  /// Note.a.inOctave(3).frequency().harmonics(upToIndex: 2)
   ///   == {const Frequency(220), const Frequency(440), const Frequency(660)}
   ///
-  /// Note.a.inOctave(5).frequency().harmonics(upTo: -2)
+  /// Note.a.inOctave(5).frequency().harmonics(upToIndex: -2)
   ///   == {const Frequency(880), const Frequency(440), const Frequency(293.33)}
   ///
-  /// Note.c.inOctave(1).frequency().harmonics(upTo: 7)
+  /// Note.c.inOctave(1).frequency().harmonics(upToIndex: 7)
   ///   .map((frequency) => frequency.closestPositionedNote().displayString())
   ///   .toSet()
   ///   == const {'C1', 'C2', 'G2+2', 'C3', 'E3-14', 'G3+2', 'A♯3-31', 'C4'}
