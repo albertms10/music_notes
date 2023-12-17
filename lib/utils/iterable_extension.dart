373fc5c84b11656a8999d2484fe0b5b1a613c401
Albert Mañosa <albertmasa2@gmail.com>
2023-12-17 07:01:57 +0100

    feat(iterable_extension): ✨ allow overriding `toNum` in `closestTo` method

--- lib/utils/iterable_extension.dart
+++ lib/utils/iterable_extension.dart
@@ -1,15 +1,26 @@
 /// An Iterable num extension.
-extension IterableNumExtension<T extends num> on Iterable<T> {
-  /// Returns the closest number to [target] in this [Iterable].
+extension IterableNumExtension<E> on Iterable<E> {
+  /// Returns the closest element to [target] in this [Iterable].
   ///
   /// Example:
   /// ```dart
   /// const [2, 5, 6, 8, 10].closestTo(7) == 6
   /// ```
-  T closestTo(T target) => reduce(
-        (closest, number) => (target - number).abs() < (target - closest).abs()
-            ? number
-            : closest,
+  E closestTo(E target, [num Function(E element)? toNum]) => reduce(
+        (closestElement, element) {
+          if (toNum == null && closestElement is! num) {
+            throw ArgumentError(
+              'Provide toNum when elements are not num',
+              'toNum',
+            );
+          }
+          final currentNum = toNum?.call(element) ?? element as num;
+          final closest = toNum?.call(closestElement) ?? closestElement as num;
+          final targetNum = toNum?.call(target) ?? target as num;
+          return (targetNum - currentNum).abs() < (targetNum - closest).abs()
+              ? element
+              : closestElement;
+        },
       );
 }
 
--- test/utils/iterable_extension_test.dart
+++ test/utils/iterable_extension_test.dart
@@ -8,11 +8,24 @@ void main() {
         expect(() => const <num>[].closestTo(1), throwsStateError);
       });
 
+      test('should throw an ArgumentError when toNum is required', () {
+        expect(
+          () => [DateTime(2025, 1, 2), DateTime(2023, 12, 15)]
+              .closestTo(DateTime.now()),
+          throwsArgumentError,
+        );
+      });
+
       test('should return the closest number to target in this Iterable', () {
         expect(const [5].closestTo(1), 5);
         expect(const [5].closestTo(-1), 5);
         expect(const [-5, 5].closestTo(0), -5);
         expect(const [2, 5, 6, 8, 10].closestTo(7), 6);
+        expect(
+          [DateTime(2025, 1, 2), DateTime(2023, 12, 15)]
+              .closestTo(DateTime(2024), (date) => date.millisecondsSinceEpoch),
+          DateTime(2023, 12, 15),
+        );
       });
     });
   });
