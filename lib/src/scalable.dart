78b3e5bd1b338cf62f9f89b14ec6ce6729b64b65
Albert Mañosa <albertmasa2@gmail.com>
2023-12-26 10:34:05 +0100

    feat(scalable): :sparkles: add `inverse`, `retrograde` getters to `ScalableIterable` (#287)
    * feat(scalable): :sparkles: add `inverse`, `retrograde` getters to `ScalableIterable`
    
    * docs(scalable): 📖 improve documentation examples
    
    * test(scalable): 🧪 add test cases for new getters
    
    * test(scalable): 🧪 add test cases for `Pitch` in `inverse`

--- lib/src/scalable.dart
+++ lib/src/scalable.dart
@@ -40,4 +40,58 @@ extension ScalableIterable<T extends Scalable<T>> on Iterable<T> {
   /// Transposes this [Iterable] by [interval].
   Iterable<T> transposeBy(Interval interval) =>
       map((item) => item.transposeBy(interval));
+
+  /// Returns the inverse of this [ScalableIterable].
+  ///
+  /// Example:
+  /// ```dart
+  /// {Note.b, Note.a.sharp, Note.d}.inverse.toSet()
+  ///   == {Note.b, Note.c, Note.g.sharp}
+  /// ```
+  Iterable<T> get inverse sync* {
+    if (isEmpty) return;
+    yield first;
+    var last = first;
+    for (var i = 1; i < length; i++) {
+      final transposed =
+          last.transposeBy(elementAt(i).interval(elementAt(i - 1)));
+      last = transposed;
+      yield transposed;
+    }
+  }
+
+  /// Returns the retrograde of this [ScalableIterable].
+  ///
+  /// Example:
+  /// ```dart
+  /// {PitchClass.dSharp, PitchClass.g, PitchClass.fSharp}.retrograde.toSet()
+  ///   == {PitchClass.fSharp, PitchClass.g, PitchClass.dSharp}
+  /// ```
+  Iterable<T> get retrograde => toList().reversed;
+
+  /// Returns the numeric representation of this [ScalableIterable].
+  ///
+  /// Example:
+  /// ```dart
+  /// {PitchClass.b, PitchClass.aSharp, PitchClass.d}
+  ///   .numericRepresentation.toSet() == {0, 11, 3}
+  /// ```
+  Iterable<int> get numericRepresentation => map(
+        (pitchClass) => first.difference(pitchClass) % chromaticDivisions,
+      );
+
+  /// Returns the delta numeric representation of this [ScalableIterable].
+  ///
+  /// Example:
+  /// ```dart
+  /// {PitchClass.b, PitchClass.aSharp, PitchClass.d, PitchClass.e}
+  ///   .deltaNumericRepresentation.toList() == const [0, -1, 4, 2]
+  /// ```
+  Iterable<int> get deltaNumericRepresentation sync* {
+    if (isEmpty) return;
+    yield 0;
+    for (var i = 1; i < length; i++) {
+      yield elementAt(i - 1).difference(elementAt(i));
+    }
+  }
 }
