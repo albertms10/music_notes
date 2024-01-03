3167e96b466363bcc4dc5a323b714e030fd32631
Albert Mañosa <albertmasa2@gmail.com>
2024-01-03 01:46:18 +0100

    refactor(frequency): ♻️ rewrite `ClosestPitch` record into a class

--- lib/src/note/frequency.dart
+++ lib/src/note/frequency.dart
@@ -5,6 +5,7 @@ part of '../../music_notes.dart';
 /// ---
 /// See also:
 /// * [Pitch].
+/// * [ClosestPitch].
 @immutable
 class Frequency implements Comparable<Frequency> {
   /// The value of this [Frequency] in Hertz.
@@ -31,17 +32,16 @@ class Frequency implements Comparable<Frequency> {
     return hertz >= minFrequency && hertz <= maxFrequency;
   }
 
-  /// Returns the closest [Pitch] to this [Frequency] from
-  /// [referenceFrequency] and [tuningSystem], with the difference in `cents`
-  /// and `hertz`.
+  /// Returns the [ClosestPitch] to this [Frequency] from [referenceFrequency]
+  /// and [tuningSystem].
   ///
   /// Example:
   /// ```dart
   /// const Frequency(467).closestPitch()
-  ///   == (Note.a.sharp.inOctave(4), cents: const Cent(3.1028), hertz: 0.8362)
+  ///   == ClosestPitch(Note.a.sharp.inOctave(4), cents: Cent(3.1), hertz: 0.84)
   ///
   /// const Frequency(260).closestPitch()
-  ///   == (Note.c.inOctave(4), cents: const Cent(-10.7903), hertz: -1.6256)
+  ///   == ClosestPitch(Note.c.inOctave(4), cents: Cent(-10.79), hertz: -1.63)
   /// ```
   ///
   /// This method and [Pitch.frequency] are inverses of each other for a
@@ -76,7 +76,7 @@ class Frequency implements Comparable<Frequency> {
         closestPitch.note.accidental == Accidental.sharp &&
             !hertzDelta.isNegative;
 
-    return (
+    return ClosestPitch(
       isCloserToUpwardsSpelling ? closestPitch.respelledUpwards : closestPitch,
       cents: Ratio(hertz / closestPitchFrequency.hertz).cents,
       hertz: hertzDelta,
@@ -108,11 +108,13 @@ class Frequency implements Comparable<Frequency> {
   /// Note.a.inOctave(5).frequency().harmonics(upToIndex: -2)
   ///   == {const Frequency(880), const Frequency(440), const Frequency(293.33)}
   ///
-  /// Note.c.inOctave(1).frequency().harmonics(upToIndex: 7)
-  ///   .map((frequency) => frequency.closestPitch().displayString())
-  ///   .toSet()
-  ///     == const {'C1', 'C2', 'G2+2', 'C3', 'E3-14', 'G3+2', 'A♯3-31', 'C4'}
+  /// Note.c.inOctave(1).frequency().harmonics(upToIndex: 7).closestPitches
+  ///     .toString() == '{C1, C2, G2+2, C3, E3-14, G3+2, A♯3-31, C4}'
   /// ```
+  ///
+  /// ---
+  /// See also:
+  /// - [FrequencyIterableExtension.closestPitches].
   Set<Frequency> harmonics({required int upToIndex}) => {
         for (var i = 0; i <= upToIndex.abs(); i++) harmonic(i * upToIndex.sign),
       };
@@ -163,24 +165,58 @@ class Frequency implements Comparable<Frequency> {
   int compareTo(Frequency other) => hertz.compareTo(other.hertz);
 }
 
-/// A record containing the closest [Pitch], with delta `cents` and `hertz`.
-typedef ClosestPitch = (Pitch closestPitch, {Cent cents, double hertz});
+/// An abstraction of the closest representation of a [Frequency] as a [Pitch].
+@immutable
+class ClosestPitch {
+  /// The pitch closest to the original [Frequency].
+  final Pitch pitch;
 
-/// A [ClosestPitch] extension.
-extension ClosestPitchExtension on ClosestPitch {
+  /// The difference in cents.
+  final Cent cents;
+
+  /// The difference in hertz.
+  final double hertz;
+
+  /// Creates a new [ClosestPitch] from [pitch], [cents] and [hertz].
+  const ClosestPitch(this.pitch, {this.cents = const Cent(0), this.hertz = 0});
+
   /// Returns the string representation of this [ClosestPitch] record.
   ///
   /// Example:
   /// ```dart
-  /// const Frequency(440).closestPitch().displayString() == 'A4'
-  /// const Frequency(98.1).closestPitch().displayString() == 'G2+2'
-  /// const Frequency(163.5).closestPitch().displayString() == 'E3-14'
-  /// const Frequency(228.9).closestPitch().displayString() == 'A♯3-31'
+  /// const Frequency(440).closestPitch().toString() == 'A4'
+  /// const Frequency(98.1).closestPitch().toString() == 'G2+2'
+  /// const Frequency(163.5).closestPitch().toString() == 'E3-14'
+  /// const Frequency(228.9).closestPitch().toString() == 'A♯3-31'
   /// ```
-  String displayString() {
+  @override
+  String toString() {
     final roundedCents = cents.value.round();
-    if (roundedCents == 0) return '${$1}';
+    if (roundedCents == 0) return '$pitch';
 
-    return '${$1}${roundedCents.toDeltaString()}';
+    return '$pitch${roundedCents.toDeltaString()}';
   }
+
+  @override
+  bool operator ==(Object other) =>
+      other is ClosestPitch &&
+      pitch == other.pitch &&
+      cents == other.cents &&
+      hertz == other.hertz;
+
+  @override
+  int get hashCode => Object.hash(pitch, cents, hertz);
+}
+
+/// A [Frequency] Iterable extension.
+extension FrequencyIterableExtension on Iterable<Frequency> {
+  /// Returns the set of [ClosestPitch] for each [Frequency] element.
+  ///
+  /// Example:
+  /// ```dart
+  /// Note.c.inOctave(1).frequency().harmonics(upToIndex: 7).closestPitches
+  ///     .toString() == '{C1, C2, G2+2, C3, E3-14, G3+2, A♯3-31, C4}'
+  /// ```
+  Set<ClosestPitch> get closestPitches =>
+      map((frequency) => frequency.closestPitch()).toSet();
 }
