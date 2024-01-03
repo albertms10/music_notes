7fd78f206bf8286e98884400afb78c2bb3176ee7
Albert Mañosa <albertmasa2@gmail.com>
2024-01-03 17:01:04 +0100

    refactor(closest_pitch): ♻️ move class to a new file (#341)

--- lib/src/note/frequency.dart
+++ lib/src/note/frequency.dart
@@ -169,49 +169,6 @@ class Frequency implements Comparable<Frequency> {
   int compareTo(Frequency other) => hertz.compareTo(other.hertz);
 }
 
-/// An abstraction of the closest representation of a [Frequency] as a [Pitch].
-@immutable
-class ClosestPitch {
-  /// The pitch closest to the original [Frequency].
-  final Pitch pitch;
-
-  /// The difference in cents.
-  final Cent cents;
-
-  /// The difference in hertz.
-  final double hertz;
-
-  /// Creates a new [ClosestPitch] from [pitch], [cents] and [hertz].
-  const ClosestPitch(this.pitch, {this.cents = const Cent(0), this.hertz = 0});
-
-  /// Returns the string representation of this [ClosestPitch] record.
-  ///
-  /// Example:
-  /// ```dart
-  /// const Frequency(440).closestPitch().toString() == 'A4'
-  /// const Frequency(98.1).closestPitch().toString() == 'G2+2'
-  /// const Frequency(163.5).closestPitch().toString() == 'E3-14'
-  /// const Frequency(228.9).closestPitch().toString() == 'A♯3-31'
-  /// ```
-  @override
-  String toString() {
-    final roundedCents = cents.value.round();
-    if (roundedCents == 0) return '$pitch';
-
-    return '$pitch${roundedCents.toDeltaString()}';
-  }
-
-  @override
-  bool operator ==(Object other) =>
-      other is ClosestPitch &&
-      pitch == other.pitch &&
-      cents == other.cents &&
-      hertz == other.hertz;
-
-  @override
-  int get hashCode => Object.hash(pitch, cents, hertz);
-}
-
 /// A [Frequency] Iterable extension.
 extension FrequencyIterableExtension on Iterable<Frequency> {
   /// Returns the set of [ClosestPitch] for each [Frequency] element.
---
+++ test/src/note/closest_pitch_test.dart
@@ -0,0 +1,91 @@
+import 'package:music_notes/music_notes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('ClosestPitch', () {
+    group('.toString()', () {
+      test('should return the string representation of this ClosestPitch', () {
+        expect(
+          ClosestPitch(Note.a.inOctave(4), cents: const Cent(3.456)).toString(),
+          'A4+3',
+        );
+        expect(
+          ClosestPitch(Note.d.flat.inOctave(3), cents: const Cent(-28.6))
+              .toString(),
+          'D♭3-29',
+        );
+        expect(
+          Note.c
+              .inOctave(1)
+              .frequency()
+              .harmonics(upToIndex: 15)
+              .closestPitches
+              .toString(),
+          '{C1, C2, G2+2, C3, E3-14, G3+2, A♯3-31, C4, '
+          'D4+4, E4-14, F♯4-49, G4+2, A♭4+41, A♯4-31, B4-12, C5}',
+        );
+      });
+    });
+
+    group('.hashCode', () {
+      test('should return the same hashCode for equal ClosestPitches', () {
+        expect(
+          ClosestPitch(Note.a.inOctave(4)),
+          ClosestPitch(Note.a.inOctave(4)),
+        );
+        expect(
+          ClosestPitch(
+            Note.c.sharp.inOctave(3),
+            cents: const Cent(-2.123),
+            hertz: -2.021,
+          ),
+          ClosestPitch(
+            Note.c.sharp.inOctave(3),
+            cents: const Cent(-2.123),
+            hertz: -2.021,
+          ),
+        );
+      });
+
+      test(
+        'should return different hashCodes for different ClosestPitches',
+        () {
+          expect(
+            ClosestPitch(Note.a.inOctave(4)),
+            isNot(equals(ClosestPitch(Note.g.inOctave(4)))),
+          );
+          expect(
+            ClosestPitch(
+              Note.c.sharp.inOctave(3),
+              cents: const Cent(-2.123),
+              hertz: -2.021,
+            ),
+            isNot(
+              equals(
+                ClosestPitch(
+                  Note.c.sharp.inOctave(3),
+                  cents: const Cent(-0.345),
+                  hertz: -4.989,
+                ),
+              ),
+            ),
+          );
+        },
+      );
+
+      test('should ignore equal ClosestPitch instances in a Set', () {
+        final collection = {
+          ClosestPitch(Note.a.inOctave(4)),
+          ClosestPitch(Note.b.flat.inOctave(3)),
+          ClosestPitch(Note.c.inOctave(3), cents: const Cent(2), hertz: 2),
+        };
+        collection.addAll(collection);
+        expect(collection.toList(), [
+          ClosestPitch(Note.a.inOctave(4)),
+          ClosestPitch(Note.b.flat.inOctave(3)),
+          ClosestPitch(Note.c.inOctave(3), cents: const Cent(2), hertz: 2),
+        ]);
+      });
+    });
+  });
+}
