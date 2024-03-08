dbf1c6ea1e48d7d3d29dd224918a8dc645c9051b
Albert Mañosa <albertmasa2@gmail.com>
2024-03-08 13:55:36 +0100

    feat(iterable_extension): ✨ allow overriding `difference` in `closestTo` method (#327)
    * feat(iterable_extension): ✨ allow overriding `toNum` in `closestTo` method
    
    * feat(iterable_extension): ✨ rewrite expecting `difference`
    
    * test(iterable_extension): 🧪 add missin `main` function to test

--- test/utils/main.dart
+++ test/utils/main.dart
@@ -1,10 +1,12 @@
 import 'int_extension_test.dart' as int_extension_test;
+import 'iterable_extension_test.dart' as iterable_extension_test;
 import 'map_extension_test.dart' as map_extension_test;
 import 'num_extension_test.dart' as num_extension_test;
 import 'string_extension_test.dart' as string_extension_test;
 
 void main() {
   int_extension_test.main();
+  iterable_extension_test.main();
   map_extension_test.main();
   num_extension_test.main();
   string_extension_test.main();
