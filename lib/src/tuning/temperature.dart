1e3dc6ac6a6a0f307510bb9381a5e2d92cebc20c
Albert Mañosa <albertmasa2@gmail.com>
2024-03-27 23:15:18 +0100

    feat(temperature): ✨ add `Celsius.zero` static constant

--- lib/src/tuning/temperature.dart
+++ lib/src/tuning/temperature.dart
@@ -1,5 +1,8 @@
 /// The representation of a Celsius temperature.
 extension type const Celsius(num degrees) implements num {
+  /// The absolute zero [Celsius] temperature.
+  static const zero = Celsius(0);
+
   /// The reference [Celsius] temperature.
   static const reference = Celsius(20);
 }
