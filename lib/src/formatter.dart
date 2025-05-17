efa76917db4be4260807cda52040c5fd59248881
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2025-05-18 01:43:37 +0200

    refactor(formatter): ♻️ remove unneeded `@mustBeOverridden` annotation
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- lib/src/formatter.dart
+++ lib/src/formatter.dart
@@ -1,15 +1,11 @@
-import 'package:meta/meta.dart' show mustBeOverridden;
-
 /// An abstract representation of a formatter for [T].
 abstract class Formatter<T> {
   /// Creates a new formatter.
   const Formatter();
 
   /// Formats this [T].
-  @mustBeOverridden
   String format(T value);
 
   /// Parses [source] as [T].
-  @mustBeOverridden
   T parse(String source);
 }
