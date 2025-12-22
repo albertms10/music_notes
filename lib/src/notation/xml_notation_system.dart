47d82399e074804405f8b91fd7db1e4d927ec10e
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2025-12-22 15:18:22 +0100

    refactor(pitch): ♻️ extract element names into static constants

--- lib/src/notation/xml_notation_system.dart
+++ lib/src/notation/xml_notation_system.dart
@@ -11,11 +11,11 @@ final class StringXmlNotationSystem<V> extends StringNotationSystem<V> {
   const StringXmlNotationSystem(this.notationSystem);
 
   @override
-  String format(V value) =>
-      notationSystem.format(value).toXmlString(pretty: true);
-
-  @override
   V parse(String source) => source.trim().isEmpty
       ? throw ArgumentError('Empty XML document.')
       : notationSystem.parse(XmlDocument.parse(source));
+
+  @override
+  String format(V value) =>
+      notationSystem.format(value).toXmlString(pretty: true);
 }
--- lib/src/note/pitch.dart
+++ lib/src/note/pitch.dart
@@ -706,35 +706,38 @@ class MusicXmlPitchNotation extends NotationSystem<Pitch, XmlDocument> {
     this.noteNameNotation = const EnglishNoteNameNotation(),
   });
 
-  @override
-  XmlDocument format(Pitch pitch) {
-    final builder = XmlBuilder();
-    builder.element(
-      'pitch',
-      nest: () {
-        builder
-          ..element('step', nest: noteNameNotation.format(pitch.note.noteName))
-          ..element('alter', nest: pitch.note.accidental.semitones)
-          ..element('octave', nest: pitch.octave);
-      },
-    );
-    return builder.buildDocument();
-  }
+  static const _pitch = 'pitch';
+  static const _step = 'step';
+  static const _alter = 'alter';
+  static const _octave = 'octave';
 
   static Never _required(String elementName) =>
       throw ArgumentError('Missing required <$elementName> element.');
 
   @override
   Pitch parse(XmlDocument source) {
-    final pitch = source.getElement('pitch') ?? _required('pitch');
-    final step = pitch.getElement('step')?.innerText ?? _required('step');
-    final octave = pitch.getElement('octave')?.innerText ?? _required('octave');
-    final alter = pitch.getElement('alter')?.innerText ?? '0';
+    final pitch = source.getElement(_pitch) ?? _required(_pitch);
+    final step = pitch.getElement(_step)?.innerText ?? _required(_step);
+    final alter = pitch.getElement(_alter)?.innerText ?? '0';
+    final octave = pitch.getElement(_octave)?.innerText ?? _required(_octave);
 
     return Pitch(
       Note(noteNameNotation.parse(step), Accidental(.parse(alter))),
       octave: .parse(octave),
     );
+  }
+
+  @override
+  XmlDocument format(Pitch pitch) {
+    final builder = XmlBuilder();
+    builder.element(
+      _pitch,
+      nest: () => builder
+        ..element(_step, nest: noteNameNotation.format(pitch.note.noteName))
+        ..element(_alter, nest: pitch.note.accidental.semitones)
+        ..element(_octave, nest: pitch.octave),
+    );
+    return builder.buildDocument();
   }
 }
 
