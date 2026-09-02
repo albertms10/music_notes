8c80194791c80914adb33ea8c053b26454bcc975
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2026-09-02 02:28:49 +0200

    refactor(pythagorean_tuning): 🔥 remove duplicated code
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- lib/src/tuning_system/pythagorean_tuning.dart
+++ lib/src/tuning_system/pythagorean_tuning.dart
@@ -1,8 +1,3 @@
-import 'dart:math' as math;
-
-import '../cent/cent.dart';
-import '../note/note.dart';
-import '../pitch/pitch.dart';
 import 'just_intonation.dart';
 
 /// A representation of the three-limit (a.k.a. Pythagorean) tuning formatter.
@@ -11,30 +6,6 @@ import 'just_intonation.dart';
 class PythagoreanTuning extends JustIntonation {
   /// Creates a new [PythagoreanTuning] from [fork].
   const PythagoreanTuning({super.fork});
-
-  @override
-  num ratio(Pitch pitch) {
-    final distance = fork.pitch.note.fifthsDistanceWith(pitch.note);
-    var ratio = 1.0;
-    for (var i = 1; i <= distance.abs(); i++) {
-      ratio *= distance.isNegative
-          ? JustIntonation.ascendingFourthRatio
-          : JustIntonation.ascendingFifthRatio;
-      // When ratio is greater than 2, so greater than [Size.octave],
-      // divide by 2 to transpose it down by one octave.
-      if (ratio >= 2) ratio /= 2;
-    }
-
-    // `ratio` is the pitch-class ratio, always within [1, 2), e.g., as if
-    // [pitch] were in the same octave as [fork]. The actual octave delta
-    // between [pitch] and [fork] is derived (with its sign) from their real
-    // semitone distance, discounting the octave already embedded in `ratio`.
-    final realCents = fork.pitch.difference(pitch) * Cent.divisionsPerSemitone;
-    final chainCents = Cent.fromRatio(ratio);
-    final octaveDelta = ((realCents - chainCents) / Cent.octave).round();
-
-    return ratio * math.pow(2, octaveDelta);
-  }
 
   /// See [Pythagorean comma](https://en.wikipedia.org/wiki/Pythagorean_comma).
   num get pythagoreanComma => ratio(fork.pitch.transposeBy(.d2.descending));
